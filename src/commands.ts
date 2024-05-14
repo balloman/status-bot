b6fb93e8a13978df6fc39dfe5398bd12c9cf2c96
Bernard Allotey <benard.allotey4@gmail.com>
2024-05-13 18:19:07 -0700

    feat: add the ability to unregister a server

--- src/commands.ts
+++ src/commands.ts
@@ -4,6 +4,7 @@ import {
   type ChatInputCommandInteraction,
   type SharedSlashCommand,
 } from "discord.js";
+import { eq } from "drizzle-orm";
 import { db } from "./db";
 import { servers } from "./schema";
 
@@ -56,6 +57,28 @@ export const COMMANDS: CommandComponents[] = [
           .map((server) => server.host)
           .join("\n")}`,
       );
+    },
+  },
+  {
+    command: new SlashCommandBuilder()
+      .setName("unregister")
+      .setDescription(
+        "Unregisters a server, this command succeeds even if the server is not registered",
+      )
+      .addStringOption((option) =>
+        option
+          .setName("server")
+          .setDescription("Server IP or hostname")
+          .setRequired(true),
+      ),
+    onExecute: async (interaction) => {
+      const host = interaction.options.getString("server");
+      if (!host) {
+        await interaction.reply("Please provide a server hostname");
+        return;
+      }
+      await db.delete(servers).where(eq(servers.host, host));
+      await interaction.reply("Unregistered server!");
     },
   },
 ];
